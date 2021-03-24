4292308a9e28ac54572423bc9aae33f60f019a3c
Md. Rakib Hossain Khan <md.rakib.hossain.khan@gmail.com>
2021-03-24 11:19:12 +0600

    Initial Commit

---
+++ Business Card/Business_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Business_CardApp.swift
+//  Business Card
+//
+//  Created by Md. Rakib Hossain Khan on 24/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Business_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
